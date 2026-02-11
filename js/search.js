abd13d006565ae0e84b5689c0ab7a52165f77fd8
thankyougesu <thankyougesu@gmail.com>
2026-02-11 21:07:14 +0900

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -1,13 +1,12 @@
 function search() {
-  const key = document.getElementById("keyword").value;
-
+  const key = document.getElementById("keyword").value
+    .trim()
+    .toLowerCase();
   if (key === "rabbit") {
     window.location.href = "answer_α.html";
   } else if (key === "clock") {
     window.location.href = "answer_β.html";
   } else {
-    window.location.href = "nosearch.html");
+    window.location.href = "nosearch.html";
   }
 }
-
-
