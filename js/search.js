79673831d9e7a8a2b3112238eaffecf3b2a99a22
thankyougesu <thankyougesu@gmail.com>
2026-03-05 20:15:06 +0900

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -6,18 +6,16 @@ function search() {
   const routes = {
     "rabbit": "answer_α.html",
     "clock": "answer_β.html",
-    "神代恒一": "president.html",
     "新入社員": "newcommer.html",
     "若村颯太": "2040801.html",
     "西村かなえ": "2040802.html",
     "林原健斗": "2040803.html",
     "児童買春": "chat01.html",
-    "新規事業": "newsection.html",
+    "新規事業部": "newsection.html",
     "都市開発事業部": "dailyblog01.html",
     "社内ブログ": "dailyblog02.html",
     "情報システム部": "securityannounce.html",
-    "\\Deus-Network\08_project": "secuSrityannounce.html",
-    "新山さん": "chat02.html",
+    "\\Deus-Network\08_project": "securityannounce.html",
   };
 
   if (routes[key]) {
