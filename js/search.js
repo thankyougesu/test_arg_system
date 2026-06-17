a1ba237b3578cd6da72ea647594f44d6f981b271
thankyougesu <thankyougesu@gmail.com>
2026-06-18 01:31:05 +0900

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -1,15 +1,8 @@
-console.log("SEARCH.JS LOADED");
-
-function normalizeText(str) {
-  return str
-    .trim()
-    .toLowerCase()
-    .normalize("NFKC");
-}
-
 async function search() {
   const input = document.getElementById("keyword").value;
   const key = normalizeText(input);
+
+  console.log("送信する文字列 =", key);
 
   try {
 
@@ -18,13 +11,17 @@ async function search() {
       encodeURIComponent(key)
     );
 
+    console.log("response status =", response.status);
+
     const data = await response.json();
+
+    console.log("Worker応答 =", data);
 
     window.location.href = data.page;
 
   } catch (error) {
 
-    console.error(error);
+    console.error("FETCH ERROR", error);
 
     window.location.href = "nosearch.html";
 
